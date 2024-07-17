a1cf4759163cf20c86167105919605b3b7ddc46d
lucasSouzaCalcme <138510417+lucasSouzaCalcme@users.noreply.github.com>
2024-07-17 14:23:02 -0300

    Removendo commit

--- hotel/src/main/java/com/ntconsult/hotel/application/ports/out/SendMessageOutputInport.java
+++
@@ -1,9 +0,0 @@
-package com.ntconsult.hotel.application.ports.out;
-
-import com.ntconsult.hotel.adapters.in.consumer.response.CustomerMessage;
-
-public interface SendMessageOutputInport {
-
-    void send(CustomerMessage msg);
-
-}
